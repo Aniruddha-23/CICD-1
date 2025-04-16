19b3462159dde65f22c1fa8cbfa39bd4cf502029
Aniruddha Ambekar <aniruddha.ambekar01@gmail.com>
2025-04-16 19:07:40 +0530

    changes in java file

--- src/test/java/CalculatorTest.java
+++ src/test/java/CalculatorTest.java
@@ -6,6 +6,6 @@ public class CalculatorTest {
 	@Test
     public void testAdd() {
         Calculator calc = new Calculator();
-        assertEquals(5, calc.add(2, 3));
+        assertEquals(6, calc.add(2, 3));
     }
 }
