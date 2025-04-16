ed96e33f94932ac12129ab1bf0638fcbf0de69d2
Aniruddha Ambekar <aniruddha.ambekar01@gmail.com>
2025-04-16 18:41:30 +0530

    minor change

--- src/test/java/CalculatorTest.java
+++ src/test/java/CalculatorTest.java
@@ -2,7 +2,6 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class CalculatorTest {
-
     @Test
     public void testAdd() {
         Calculator calc = new Calculator();
